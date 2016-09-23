d566627f053f15df64ff1297d7002138c4f2eb3c
shanedrgn <shanedrgn@gmail.com>
2016-09-23 07:56:54 -0500

    date

--- github_login.bat
+++ github_login.bat
@@ -1,2 +1,7 @@
 git config --global user.name "shanedrgn"
 git config --global user.email "shanedrgn@gmail.com"
+git add *
+$date = get-date -format "{dd-MMM-yyyy HH:mm}"
+git commit -m date
+git push
+shanedrgn
