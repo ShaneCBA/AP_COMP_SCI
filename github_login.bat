e9e2d7382cb5032c77fbe06f838738b11039d4aa
shanedrgn <shanedrgn@gmail.com>
2016-12-05 08:06:58 -0600

    2016-12-05

--- github_login.bat
+++ github_login.bat
@@ -5,4 +5,5 @@ for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined MyDate se
 set today=%MyDate:~0,4%-%MyDate:~4,2%-%MyDate:~6,2%
 git commit -m %today%
 git push
-shanedrgn
+echo %today%
+echo !%today%!
